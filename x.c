5007433a3ead926fb915bc7e2a5be7f73652aa95
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2020-04-06 09:43:50 -0400

    Added clipboard patch

--- x.c
+++ x.c
@@ -637,6 +637,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
@@ -654,7 +656,7 @@ brelease(XEvent *e)
 	}
 
 	if (e->xbutton.button == Button2)
-		selpaste(NULL);
+		clippaste(NULL);
 	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
