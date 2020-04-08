2846db523520ee8839bcf680b5144ea2f144ce04
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2020-04-08 00:38:56 -0400

    Revert "Merge branch 'tmp2'"
    This reverts commit e84b310190a26831501e5ea529b3fe3ad4db3261, reversing
    changes made to a54488e62f087aed47b5a9f26bc36016d521c094.

--- st.c
+++ st.c
@@ -166,7 +166,6 @@ static void sigchld(int);
 static void ttywriteraw(const char *, size_t);
 
 static void csidump(void);
-static void dcshandle(void);
 static void csihandle(void);
 static void csiparse(void);
 static void csireset(void);
@@ -1028,7 +1027,6 @@ void
 treset(void)
 {
 	uint i;
-	ImageList *im;
 
 	term.c = (TCursor){{
 		.mode = ATTR_NULL,
@@ -1051,9 +1049,6 @@ treset(void)
 		tclearregion(0, 0, term.col-1, term.row-1);
 		tswapscreen();
 	}
-
-	for (im = term.images; im; im = im->next)
-		im->should_delete = 1;
 }
 
 void
@@ -1088,12 +1083,9 @@ void
 tswapscreen(void)
 {
 	Line *tmp = term.line;
-	ImageList *im = term.images;
 
 	term.line = term.alt;
 	term.alt = tmp;
-	term.images = term.images_alt;
-	term.images_alt = im;
 	term.mode ^= MODE_ALTSCREEN;
 	tfulldirt();
 }
@@ -1134,7 +1126,6 @@ tscrolldown(int orig, int n, int copyhist)
 {
 	int i;
 	Line temp;
-	ImageList *im;
 
 	LIMIT(n, 0, term.bot-orig+1);
 
@@ -1154,13 +1145,6 @@ tscrolldown(int orig, int n, int copyhist)
 		term.line[i-n] = temp;
 	}
 
-	for (im = term.images; im; im = im->next) {
-		if (im->y < term.bot)
-			im->y += n;
-		if (im->y > term.bot)
-			im->should_delete = 1;
-	}
-
 	selscroll(orig, n);
 }
 
@@ -1169,7 +1153,6 @@ tscrollup(int orig, int n, int copyhist)
 {
 	int i;
 	Line temp;
-	ImageList *im;
 
 	LIMIT(n, 0, term.bot-orig+1);
 
@@ -1190,13 +1173,6 @@ tscrollup(int orig, int n, int copyhist)
 		temp = term.line[i];
 		term.line[i] = term.line[i+n];
 		term.line[i+n] = temp;
-	}
-
-	for (im = term.images; im; im = im->next) {
-		if (im->y+im->height/win.ch > term.top)
-			im->y -= n;
-		if (im->y+im->height/win.ch < term.top)
-			im->should_delete = 1;
 	}
 
 	selscroll(orig, -n);
@@ -1714,23 +1690,6 @@ tsetmode(int priv, int set, int *args, int narg)
 }
 
 void
-dcshandle(void)
-{
-	switch (csiescseq.mode[0]) {
-	default:
-		fprintf(stderr, "erresc: unknown csi ");
-		csidump();
-		/* die(""); */
-		break;
-	case 'q': /* DECSIXEL */
-		if (sixel_parser_init(&sixel_st, 0, 0 << 16 | 0 << 8 | 0, 1, win.cw, win.ch) != 0)
-			perror("sixel_parser_init() failed");
-		term.mode |= MODE_SIXEL;
-		break;
-	}
-}
-
-void
 csihandle(void)
 {
 	char buf[40];
@@ -1971,8 +1930,6 @@ strhandle(void)
 {
 	char *p = NULL;
 	int j, narg, par;
-	ImageList *new_image;
-	int i;
 
 	term.esc &= ~(ESC_STR_END|ESC_STR);
 	strparse();
@@ -2023,39 +1980,7 @@ strhandle(void)
 		xsettitle(strescseq.args[0]);
 		return;
 	case 'P': /* DCS -- Device Control String */
-		if (IS_SET(MODE_SIXEL)) {
-			term.mode &= ~MODE_SIXEL;
-			new_image = malloc(sizeof(ImageList));
-			memset(new_image, 0, sizeof(ImageList));
-			new_image->x = term.c.x;
-			new_image->y = term.c.y;
-			new_image->width = sixel_st.image.width;
-			new_image->height = sixel_st.image.height;
-			new_image->pixels = malloc(new_image->width * new_image->height * 4);
-			if (sixel_parser_finalize(&sixel_st, new_image->pixels) != 0) {
-				perror("sixel_parser_finalize() failed");
-				sixel_parser_deinit(&sixel_st);
-				return;
-			}
-			sixel_parser_deinit(&sixel_st);
-			if (term.images) {
-				ImageList *im;
-				for (im = term.images; im->next;)
-					im = im->next;
-				im->next = new_image;
-				new_image->prev = im;
-			} else {
-				term.images = new_image;
-			}
-			for (i = 0; i < (sixel_st.image.height + win.ch-1)/win.ch; ++i) {
-				int x;
-				tclearregion(term.c.x, term.c.y, term.c.x+(sixel_st.image.width+win.cw-1)/win.cw, term.c.y);
-				for (x = term.c.x; x < MIN(term.col, term.c.x+(sixel_st.image.width+win.cw-1)/win.cw); x++)
-					term.line[term.c.y][x].mode |= ATTR_SIXEL;
-				tnewline(1);
-			}
-		}
-		return;
+		term.mode |= ESC_DCS;
 	case '_': /* APC -- Application Program Command */
 	case '^': /* PM -- Privacy Message */
 		return;
@@ -2381,7 +2306,6 @@ eschandle(uchar ascii)
 		term.esc |= ESC_UTF8;
 		return 0;
 	case 'P': /* DCS -- Device Control String */
-		term.esc |= ESC_DCS;
 	case '_': /* APC -- Application Program Command */
 	case '^': /* PM -- Privacy Message */
 	case ']': /* OSC -- Operating System Command */
@@ -2484,18 +2408,22 @@ tputc(Rune u)
 		if (u == '\a' || u == 030 || u == 032 || u == 033 ||
 		   ISCONTROLC1(u)) {
 			term.esc &= ~(ESC_START|ESC_STR|ESC_DCS);
+			if (IS_SET(MODE_SIXEL)) {
+				/* TODO: render sixel */;
+				term.mode &= ~MODE_SIXEL;
+				return;
+			}
 			term.esc |= ESC_STR_END;
 			goto check_control_code;
 		}
 
 
 		if (IS_SET(MODE_SIXEL)) {
-			if (sixel_parser_parse(&sixel_st, (unsigned char *)&u, 1) != 0)
-				perror("sixel_parser_parse() failed");
+			/* TODO: implement sixel mode */
 			return;
 		}
-		if (term.esc & ESC_DCS)
-			goto check_control_code;
+		if (term.esc&ESC_DCS && strescseq.len == 0 && u == 'q')
+			term.mode |= MODE_SIXEL;
 
 		if (strescseq.len+len >= sizeof(strescseq.buf)-1) {
 			/*
@@ -2540,15 +2468,6 @@ check_control_code:
 				term.esc = 0;
 				csiparse();
 				csihandle();
-			}
-			return;
-		} else if (term.esc & ESC_DCS) {
-			csiescseq.buf[csiescseq.len++] = u;
-			if (BETWEEN(u, 0x40, 0x7E)
-					|| csiescseq.len >= \
-					sizeof(csiescseq.buf)-1) {
-				csiparse();
-				dcshandle();
 			}
 			return;
 		} else if (term.esc & ESC_UTF8) {
